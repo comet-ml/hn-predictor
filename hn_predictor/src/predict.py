36935b2714203b4ef3108722438c8f7431c7daae
DN6 <dhruv.nair@gmail.com>
2022-01-13 23:41:33 +0530

    update train and predict scripts to use new dataset artifact fetching

--- hn_predictor/src/predict.py
+++ hn_predictor/src/predict.py
@@ -5,7 +5,7 @@ import argparse
 
 from data import load_data, fetch_dataset_artifact
 
-DATA_PATH = os.getenv("DATA_PATH")
+DATA_PATH = os.getenv("DATA_PATH", "../data")
 
 
 def get_args():
@@ -34,7 +34,7 @@ def main():
         artifact_assets=[args.asset_name],
         output_path=DATA_PATH,
     )
-    df = load_data(path)
+    df = load_data(dataset_config[args.asset_split_name])
 
     # TODO: Run predictions on the given dataset
 
--- hn_predictor/src/train.py
+++ hn_predictor/src/train.py
@@ -3,17 +3,15 @@ import comet_ml
 import os
 import argparse
 
-from data import load_data, fetch_artifact
+from data import load_data, fetch_dataset_artifact
 
-DATA_PATH = os.getenv("DATA_PATH")
+DATA_PATH = os.getenv("DATA_PATH", "../data")
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--train_artifact_name", type=str)
-    parser.add_argument("--train_artifact_version", type=str)
-    parser.add_argument("--valid_artifact_name", type=str)
-    parser.add_argument("--valid_artifact_version", type=str)
+    parser.add_argument("--artifact_name", type=str)
+    parser.add_argument("--artifact_version", type=str)
     parser.add_argument("--message", type=str)
 
     return parser.parse_args()
@@ -25,15 +23,15 @@ def main():
     experiment = comet_ml.Experiment()
     experiment.add_tag("train_and_evaluate")
 
-    train_path = fetch_artifact(
-        DATA_PATH, args.train_artifact_name, args.train_artifact_version
+    dataset_config = fetch_dataset_artifact(
+        experiment,
+        args.train_artifact_name,
+        args.train_artifact_version,
+        output_path=DATA_PATH,
     )
-    train_df = load_data(train_path)
 
-    valid_path = fetch_artifact(
-        DATA_PATH, args.valid_artifact_name, args.valid_artifact_version
-    )
-    valid_df = load_data(valid_path)
+    train_df = load_data(dataset_config["train"])
+    valid_df = load_data(dataset_config["valid"])
 
     # TODO: Model training and evaluation
 
