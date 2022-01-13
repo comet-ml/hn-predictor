6b8e9d12f84543f59021a8084b989cfe9b92e241
DN6 <dhruv.nair@gmail.com>
2022-01-13 23:32:12 +0530

    change name of profiling script

--- hn_predictor/src/predict.py
+++ hn_predictor/src/predict.py
@@ -3,7 +3,7 @@ import comet_ml
 import os
 import argparse
 
-from data import load_data, fetch_artifact
+from data import load_data, fetch_dataset_artifact
 
 DATA_PATH = os.getenv("DATA_PATH")
 
@@ -14,6 +14,8 @@ def get_args():
     parser.add_argument("--model_version", type=str)
     parser.add_argument("--artifact_name", type=str)
     parser.add_argument("--artifact_version", type=str)
+    parser.add_argument("--asset_split_name", type=str)
+    parser.add_argument("--asset_name", type=str)
     parser.add_argument("--message", type=str)
 
     return parser.parse_args()
@@ -25,8 +27,12 @@ def main():
     experiment = comet_ml.Experiment()
     experiment.add_tag("prediction")
 
-    path = fetch_artifact(
-        DATA_PATH, args.train_artifact_name, args.train_artifact_version
+    dataset_config = fetch_dataset_artifact(
+        experiment,
+        args.train_artifact_name,
+        args.train_artifact_version,
+        artifact_assets=[args.asset_name],
+        output_path=DATA_PATH,
     )
     df = load_data(path)
 
