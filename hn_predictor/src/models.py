8fbf3c11df306e090fb867a49d680807b9d1a122
DN6 <dhruv.nair@gmail.com>
2022-01-17 17:21:11 +0530

    add baseline dummy regressor

--- hn_predictor/src/models.py
+++ hn_predictor/src/models.py
@@ -1,1 +1,23 @@
-# TODO: Define Model class
+import pickle
+from sklearn.dummy import DummyRegressor
+
+
+class BaselineModel:
+    def __init__(self, strategy="mean", quantile=0.5) -> None:
+        if strategy == "quantile":
+            self.model = DummyRegressor(strategy, quantile=quantile)
+
+        else:
+            self.model = DummyRegressor(strategy)
+
+    def fit(self, X, y):
+        self.model.fit(X, y)
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+    def load(self, path):
+        self.model = pickle.load(path)
+
+    def save(self, path):
+        self.model.save(path)
