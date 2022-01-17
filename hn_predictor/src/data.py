af037e0001586b43cfb0b01fa64d5ad612974057
DN6 <dhruv.nair@gmail.com>
2022-01-17 16:25:18 +0530

    profile all assets in artifact

--- hn_predictor/src/data.py
+++ hn_predictor/src/data.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 import sweetviz as sv
 
+OVERWRITE_STRATEGY = os.getenv("COMET_ARTIFACT_OVERWRITE_STRATEGY", "OVERWRITE")
+
 
 def load_data(data_path):
     return pd.read_pickle(data_path)
@@ -10,17 +12,21 @@ def load_data(data_path):
 def fetch_dataset_artifact(
     experiment, artifact_name, artifact_version, artifact_assets=[], output_path="./"
 ):
-    artifact = experiment.get_artifact(artifact_name, artifact_version)
+    artifact = experiment.get_artifact(
+        artifact_name, workspace=experiment.workspace, version_or_alias=artifact_version
+    )
     metadata = artifact.metadata
 
     if not artifact_assets:
         # Download all data from the Artifact
-        artifact.download(output_path)
+        artifact.download(output_path, overwrite_strategy=OVERWRITE_STRATEGY)
 
     else:
         # Download specific assets in the Artifact
         for asset in artifact_assets:
-            artifact.get_asset(asset).download(output_path)
+            artifact.get_asset(asset).download(
+                output_path, overwrite_strategy=OVERWRITE_STRATEGY
+            )
 
     output = {}
     for key, value in metadata["filenames"].items():
@@ -29,6 +35,11 @@ def fetch_dataset_artifact(
     return output
 
 
-def profile_data(df, experiment):
-    report = sv.analyze(df)
+def profile_data(experiment, dataframes, target=None):
+    filtered = []
+    for df, label in dataframes:
+        df = df.dropna(subset=[target])
+        filtered.append([df, label])
+
+    report = sv.compare(*filtered, target_feat=target)
     report.log_comet(experiment)
--- hn_predictor/src/profile_df.py
+++ hn_predictor/src/profile_df.py
@@ -3,15 +3,18 @@ import comet_ml
 import os
 import argparse
 
-from data import load_data, fetch_artifact, profile_data
+from data import load_data, fetch_dataset_artifact, profile_data
 
-DATA_PATH = os.getenv("DATA_PATH")
+DATA_PATH = os.getenv("DATA_PATH", "../data")
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--artifact_name", type=str)
-    parser.add_argument("--artifact_version", type=str)
+    parser.add_argument("--artifact_version", type=str, default="latest")
+    parser.add_argument("--asset_names")
+    parser.add_argument("--asset_split_name", type=str)
+    parser.add_argument("--target_name", type=str)
     parser.add_argument("--message", type=str)
 
     return parser.parse_args()
@@ -23,13 +26,17 @@ def main():
     experiment = comet_ml.Experiment()
     experiment.add_tag("profile")
 
-    path = fetch_artifact(
-        DATA_PATH, args.train_artifact_name, args.train_artifact_version
+    data_config = fetch_dataset_artifact(
+        experiment,
+        artifact_name=args.artifact_name,
+        artifact_version=args.artifact_version,
+        output_path=DATA_PATH,
     )
-    df = load_data(path)
-    profile_data(df, experiment)
-
-    return
+    datasets = [
+        [load_data(path), split_name] for split_name, path in data_config.items()
+    ]
+    # df = load_data(data_config[args.asset_split_name])
+    profile_data(experiment, datasets, args.target_name)
 
 
 if __name__ == "__main__":
