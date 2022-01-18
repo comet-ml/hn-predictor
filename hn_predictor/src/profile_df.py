3cdf68944b7132e937312c714bccd89ad7ca6c1b
DN6 <dhruv.nair@gmail.com>
2022-01-18 21:15:41 +0530

    update dataset profile script

--- hn_predictor/src/profile_df.py
+++ hn_predictor/src/profile_df.py
@@ -3,15 +3,17 @@ import comet_ml
 import os
 import argparse
 
-from data import load_data, fetch_artifact, profile_data
+from data import load_data, fetch_dataset_artifact, profile_data
 
-DATA_PATH = os.getenv("DATA_PATH")
+DATA_PATH = os.getenv("DATA_PATH", "../data")
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--artifact_name", type=str)
-    parser.add_argument("--artifact_version", type=str)
+    parser.add_argument("--artifact_version", type=str, default="latest")
+    parser.add_argument("--minimal", action="store_true")
+    parser.add_argument("--log_raw_dataframe", action="store_true")
     parser.add_argument("--message", type=str)
 
     return parser.parse_args()
@@ -23,13 +25,21 @@ def main():
     experiment = comet_ml.Experiment()
     experiment.add_tag("profile")
 
-    path = fetch_artifact(
-        DATA_PATH, args.train_artifact_name, args.train_artifact_version
+    data_config = fetch_dataset_artifact(
+        experiment,
+        artifact_name=args.artifact_name,
+        artifact_version=args.artifact_version,
+        output_path=DATA_PATH,
     )
-    df = load_data(path)
-    profile_data(df, experiment)
-
-    return
+    datasets = [
+        [load_data(path), split_name] for split_name, path in data_config.items()
+    ]
+    profile_data(
+        experiment,
+        datasets,
+        minimal=True if args.minimal else False,
+        log_raw_dataframe=True if args.log_raw_dataframe else False,
+    )
 
 
 if __name__ == "__main__":
