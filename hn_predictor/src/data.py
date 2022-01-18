912e04e21256f19b5c8069bbe45dd5161eae26ae
DN6 <dhruv.nair@gmail.com>
2022-01-18 23:33:15 +0530

    remove unused imports

--- hn_predictor/src/data.py
+++ hn_predictor/src/data.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
-import sweetviz as sv
+
+
+OVERWRITE_STRATEGY = os.getenv("COMET_ARTIFACT_OVERWRITE_STRATEGY", "OVERWRITE")
 
 
 def load_data(data_path):
@@ -10,17 +12,21 @@ def load_data(data_path):
 def fetch_dataset_artifact(
     experiment, artifact_name, artifact_version, artifact_assets=[], output_path="./"
 ):
-    artifact = experiment.get_artifact(artifact_name, artifact_version)
+    artifact = experiment.get_artifact(
+        artifact_name, workspace=experiment.workspace, version_or_alias=artifact_version
+    )
     metadata = artifact.metadata
 
     if not artifact_assets:
         # Download all data from the Artifact
-        artifact.download(output_path)
+        artifact.download(output_path, overwrite_strategy=OVERWRITE_STRATEGY)
 
     else:
         # Download specific assets in the Artifact
         for asset in artifact_assets:
-            artifact.get_asset(asset).download(output_path)
+            artifact.get_asset(asset).download(
+                output_path, overwrite_strategy=OVERWRITE_STRATEGY
+            )
 
     output = {}
     for key, value in metadata["filenames"].items():
@@ -29,6 +35,8 @@ def fetch_dataset_artifact(
     return output
 
 
-def profile_data(df, experiment):
-    report = sv.analyze(df)
-    report.log_comet(experiment)
+def profile_data(experiment, dataframes, minimal=True, log_raw_dataframe=False):
+    for df, label in dataframes:
+        experiment.log_dataframe_profile(
+            df, label, minimal=minimal, log_raw_dataframe=log_raw_dataframe
+        )
